f52162cf47277d5a086d32e73d320a793edd8bc5
ZeynepUzmez <83713406+ZeynepUzmez@users.noreply.github.com>
2023-06-03 17:39:10 +0300

    #18 Kayıt Ol Ekranı-Uyarı ve bildirimlerin biçim ve konumlandırılması.
    #18 Kayıt Ol Ekranı-Uyarı ve bildirimlerin biçim ve konumlandırılması.

---
+++ src/utils/authErrorMessageParser.js
@@ -0,0 +1,24 @@
+export default function(errorCode){
+    switch (errorCode) {
+        case 'auth/invalid-email':
+            return 'Geçersiz E-Posta Adresi!';
+
+        case 'auth/email-already-exists':
+            return 'Kullanıcı Zaten Kayıtlı!';
+
+        case 'auth/user-not-found':
+            return 'Kullanıcı Bulunamadı!';
+    
+        case 'auth/weak-password':
+            return 'Parolanız Zayıf!';
+        
+        case 'auth/wrong-password':
+            return 'Hatalı Şifre Girişi';
+
+        case 'auth/unknown':
+            return 'Bilinmeyen Erişim Doğrulaması!';
+
+        default:
+            return errorCode;
+    };
+};
