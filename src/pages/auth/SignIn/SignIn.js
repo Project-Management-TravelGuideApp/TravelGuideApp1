50d983a4be1f25b659ba301ee704ab0cc5fe2176
ZeynepUzmez <83713406+ZeynepUzmez@users.noreply.github.com>
2023-06-03 17:33:45 +0300

    #17 Kayıt Ol Ekranı-Text'lerin, butonun ve input'ların biçim ve konumlandırılması.
    #17 Kayıt Ol Ekranı-Text'lerin, butonun ve input'ların biçim ve konumlandırılması.

--- src/pages/auth/SignIn/SignIn.js
+++ src/pages/auth/SignIn/SignIn.js
@@ -1,10 +1,50 @@
 import {Text, View} from 'react-native';
 import React from 'react';
+import styles from './SignIn.styles';
 
 const SignIn = () => {
   return (
-    <View>
-      <Text>SignIn</Text>
+    <View style={styles.container}>
+      <Text style={styles.header}>Kayıt Ekranı</Text>
+
+
+            <Text style={styles.lineStyle}>Adınız</Text>
+            <Input
+              placeholder="Adınız"
+            />
+            {console.log('username:',values.username)}
+            <Text style={styles.lineStyle}>Soyadınız</Text>
+            <Input
+              placeholder="Soyadınız"
+            />
+            {console.log('surname:',values.surname)}
+            <Text style={styles.lineStyle}>E-Mailiniz</Text>
+            <Input
+              placeholder="E-Mailiniz"
+              type="email"
+              
+            />
+             {console.log('mail:',values.usermail)}
+            <Text style={styles.lineStyle}>Şifreniz</Text>
+            <Input
+              value={values.password}
+              onType={handleChange('password')}
+              placeholder="Şifreniz"
+              secureTextEntry={true}
+            />
+            {console.log('password:',values.password)}
+            <Text style={styles.lineStyle}>Şifrenizin Tekrarı</Text>
+            <Input
+              placeholder="Şifrenizin Tekrarı"
+            />
+            {console.log('againPassword:',values.againPassword)}
+            <Button 
+              text="Kayıt Ol" 
+              loading={loading} 
+            />
+
+        
+
     </View>
   );
 };
--- src/pages/auth/SignIn/SignIn.styles.js
+++ src/pages/auth/SignIn/SignIn.styles.js
@@ -1,5 +1,23 @@
-import {StyleSheet, Dimensions} from 'react-native';
+import { StyleSheet } from "react-native";
 
 export default StyleSheet.create({
+    container:{
 
+    },
+    header:{
+        fontFamily:'PalanquinDark-Regular',
+        fontSize:30,
+        alignSelf:'center',
+        marginTop:25,
+        color:'#ffd633',
+        fontWeight:'bold'
+    },
+    lineStyle:{
+        marginHorizontal:12,
+        marginVertical:10,
+        fontFamily:'RopaSans-Regular',
+        fontWeight:'bold',
+        fontSize:20,
+        color:'#737373'  
+    }
 });
