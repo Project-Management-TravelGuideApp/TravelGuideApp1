0c0337b5f5ab1ea4c3c40c2cbb95349559c4651e
ZeynepUzmez <83713406+ZeynepUzmez@users.noreply.github.com>
2023-06-06 21:16:19 +0300

    #27 Giriş Yap Ekranı - Text biçimlendirilmesi ve konumlandırılması.
    #27 Giriş Yap Ekranı - Text biçimlendirilmesi ve konumlandırılması.

--- src/pages/auth/Login/Login.styles.js
+++ src/pages/auth/Login/Login.styles.js
@@ -1,5 +1,32 @@
 import {StyleSheet, Dimensions} from 'react-native';
 
 export default StyleSheet.create({
+    container:{
 
+    },
+    header:{
+        fontFamily:'PalanquinDark-Regular',
+        fontSize:30,
+        alignSelf:'center',
+        marginTop:30,
+        color:'#ffd633',
+        fontWeight:'bold'
+    },
+    body_container:{
+        marginVertical:100
+    },
+    lineStyle:{
+        marginHorizontal:12,
+        marginVertical:10,
+        fontFamily:'RopaSans-Regular',
+        fontWeight:'bold',
+        fontSize:20,
+        color:'#737373' 
+    },
+    question_text:{
+        fontSize:18,
+        fontWeight:'bold',
+        alignSelf:'center',
+        color:'#737373',
+    }
 });
