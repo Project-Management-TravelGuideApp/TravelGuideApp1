a19e0116b33dd4751f3a0c7b18cb7918e081702e
Özlem Miğfer <ozlemhayalkurun@gmail.com>
2023-06-06 21:58:10 +0300

    #29 Giriş Yap Ekranı - Kullanıcının girdi değerlerini formValues formatında kullanıcıdan alınması.
    #29 Giriş Yap Ekranı - Kullanıcının girdi değerlerini formValues formatında kullanıcıdan alınması.

--- src/pages/auth/Login/Login.js
+++ src/pages/auth/Login/Login.js
@@ -3,38 +3,51 @@ import React, {useState} from 'react';
 import styles from './Login.styles';
 import Input from '../../../components/Input';
 import Button from '../../../components/Button';
-import { showMessage } from "react-native-flash-message";
-import authErrorMessageParser from "../../../utils/authErrorMessageParser";
+import {showMessage} from 'react-native-flash-message';
+import authErrorMessageParser from '../../../utils/authErrorMessageParser';
+import {Formik} from 'formik';
+import auth from '@react-native-firebase/auth';
 
 const Login = () => {
   return (
     <View style={styles.container}>
-        <Text style={styles.header}>Giriş Ekranı</Text>
+      <Text style={styles.header}>Giriş Ekranı</Text>
 
-        <View style={styles.body_container}>
-                <Text style={styles.lineStyle}>E-Mailiniz</Text>
-                <Input 
-                    placeholder="E-Mailiniz"
-                    autoCorrect={false} 
-                    type="email"
-                />
-                <Text style={styles.lineStyle}>Şifreniz</Text>
-                <Input
-                    placeholder="Şifreniz"
-                    secureTextEntry={true}
-                    autoCorrect={false}
-                />
-                <Button 
-                    text="Giriş Yap" 
-                    loading={loading}
-                />
-                <TouchableOpacity
-                  loading={loading}
-                >
-                  <Text style={styles.question_text}>Şifremi unuttum</Text>
-                </TouchableOpacity>
-        </View>
+      <View style={styles.body_container}>
+        <Formik initialValues={initialFormValues} onSubmit={handleFormSubmit}>
+          {({values, handleChange, handleSubmit}) => (
+            <>
+              <Text style={styles.lineStyle}>E-Mailiniz</Text>
+              <Input
+                value={values.usermail}
+                onType={handleChange('usermail')}
+                placeholder="E-Mailiniz"
+                autoCorrect={false}
+                type="email"
+              />
+              <Text style={styles.lineStyle}>Şifreniz</Text>
+              <Input
+                value={values.password}
+                onType={handleChange('password')}
+                placeholder="Şifreniz"
+                secureTextEntry={true}
+                autoCorrect={false}
+              />
+              <Button
+                text="Giriş Yap"
+                loading={loading}
+                onPress={handleSubmit}
+              />
+              <TouchableOpacity
+                onPress={() => navigation.navigate('ForgotPassword')}
+                loading={loading}>
+                <Text style={styles.question_text}>Şifremi unuttum</Text>
+              </TouchableOpacity>
+            </>
+          )}
+        </Formik>
       </View>
+    </View>
   );
 };
 
