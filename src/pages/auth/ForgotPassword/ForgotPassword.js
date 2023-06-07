206094929c4bb8f1ffced3e36d6e0a582d95a10e
Özlem Miğfer <ozlemhayalkurun@gmail.com>
2023-06-07 15:41:38 +0300

    #37 Şifremi Unuttum Ekranı-Kullanıcıdan kayıtlı mailinin formValues formatında alınması.
    #37 Şifremi Unuttum Ekranı-Kullanıcıdan kayıtlı mailinin formValues formatında alınması.

--- src/pages/auth/ForgotPassword/ForgotPassword.js
+++ src/pages/auth/ForgotPassword/ForgotPassword.js
@@ -3,26 +3,33 @@ import {Text, View} from 'react-native';
 import styles from './ForgotPassword.styles';
 import Input from '../../../components/Input';
 import Button from '../../../components/Button';
-import { showMessage } from "react-native-flash-message";
-import authErrorMessageParser from "../../../utils/authErrorMessageParser";
+import {showMessage} from 'react-native-flash-message';
+import authErrorMessageParser from '../../../utils/authErrorMessageParser';
+import {Formik} from 'formik';
 
 const ForgotPassword = () => {
-    return(
-        <View style={styles.container}>
-            <Text style={styles.header}>Şifremi Unuttum</Text>
-            <View style={styles.body_container}>
-                <Text style={styles.lineStyle}>E-Mailiniz</Text>
-                            <Input
-                                placeholder="E-Mailiniz"
-                                autoCorrect={false}
-                                type="email"
-                            />
-                            <Button
-                                text="Gönder"
-                            />             
-            </View> 
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+      <Text style={styles.header}>Şifremi Unuttum</Text>
+      <View style={styles.body_container}>
+        <Text style={styles.lineStyle}>E-Mailiniz</Text>
+        <Formik initialValues={initialFormValues} onSubmit={handleFormSubmit}>
+          {({values, handleChange, handleSubmit}) => (
+            <>
+              <Input
+                value={values.mail}
+                placeholder="E-Mailiniz"
+                onType={handleChange('mail')}
+                autoCorrect={false}
+                type="email"
+              />
+              <Button onPress={handleSubmit} text="Gönder" />
+            </>
+          )}
+        </Formik>
+      </View>
+    </View>
+  );
 };
 
 export default ForgotPassword;
