c6273f4c87d3abb828bacb07beff94428281ca43
Özlem Miğfer <ozlemhayalkurun@gmail.com>
2023-06-07 15:45:59 +0300

    #38 Şifremi Unuttum Ekranı-Kayıtlı olmayan mail girişiyle uyarı mesajının verilmesi.
    #38 Şifremi Unuttum Ekranı-Kayıtlı olmayan mail girişiyle uyarı mesajının verilmesi.

--- src/pages/auth/ForgotPassword/ForgotPassword.js
+++ src/pages/auth/ForgotPassword/ForgotPassword.js
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Text, View} from 'react-native';
 import styles from './ForgotPassword.styles';
 import Input from '../../../components/Input';
 import Button from '../../../components/Button';
 import {showMessage} from 'react-native-flash-message';
 import authErrorMessageParser from '../../../utils/authErrorMessageParser';
+import auth from '@react-native-firebase/auth';
 import {Formik} from 'formik';
 
+const initialFormValues = {
+  mail: '',
+};
+
 const ForgotPassword = () => {
+  const [mail, setMail] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  async function handleFormSubmit(formValues) {
+    try {
+      setLoading(true);
+      
+    } catch (error) {
+      console.log(error);
+      showMessage({
+        message: authErrorMessageParser(error.code),
+        type: 'danger',
+      });
+      setLoading(false);
+    }
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.header}>Şifremi Unuttum</Text>
