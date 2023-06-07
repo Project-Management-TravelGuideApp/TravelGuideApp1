00d195bcac0ec7a09849af4d1156ce10c7313fb7
Özlem Miğfer <ozlemhayalkurun@gmail.com>
2023-06-07 15:55:12 +0300

    #36 Şifremi Unuttum Ekranı-Uyarı ve bildirimlerin biçim ve konumlandırılması.
    #36 Şifremi Unuttum Ekranı-Uyarı ve bildirimlerin biçim ve konumlandırılması.

--- src/pages/auth/ForgotPassword/ForgotPassword.styles.js
+++ src/pages/auth/ForgotPassword/ForgotPassword.styles.js
@@ -1,5 +1,26 @@
-import {StyleSheet, Dimensions} from 'react-native';
+import { StyleSheet } from "react-native";
 
 export default StyleSheet.create({
+    container:{
 
+    },
+    header:{
+        fontFamily:'PalanquinDark-Regular',
+        fontSize:30,
+        alignSelf:'center',
+        marginTop:30,
+        color:'#ffd633',
+        fontWeight:'bold'
+    },
+    body_container:{
+        marginVertical:180
+    },
+    lineStyle:{
+        marginHorizontal:12,
+        marginVertical:10,
+        fontFamily:'RopaSans-Regular',
+        fontWeight:'bold',
+        fontSize:20,
+        color:'#737373' 
+    },
 });
