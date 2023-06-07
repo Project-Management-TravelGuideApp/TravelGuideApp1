3c90173cf8a022d04f10dedece4fb4fc14458f06
ZeynepUzmez <83713406+ZeynepUzmez@users.noreply.github.com>
2023-06-07 15:36:15 +0300

    #35 Şifremi Unuttum Ekranı-Input&buton componentlerin çağrılıp eklenmesi.
    #35 Şifremi Unuttum Ekranı-Input&buton componentlerin çağrılıp eklenmesi.

--- src/pages/auth/ForgotPassword/ForgotPassword.js
+++ src/pages/auth/ForgotPassword/ForgotPassword.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import {Text, View} from 'react-native';
+import styles from './ForgotPassword.styles';
+import Input from '../../../components/Input';
+import Button from '../../../components/Button';
 
 const ForgotPassword = () => {
     return(
-        <View>
-            <Text>ForgotPassword</Text>
+        <View style={styles.container}>
+            <Text style={styles.header}>Şifremi Unuttum</Text>
+            <View style={styles.body_container}>
+                <Text style={styles.lineStyle}>E-Mailiniz</Text>
+                            <Input
+                                placeholder="E-Mailiniz"
+                                autoCorrect={false}
+                                type="email"
+                            />
+                            <Button
+                                text="Gönder"
+                            />             
+            </View> 
         </View>
     );
 };
