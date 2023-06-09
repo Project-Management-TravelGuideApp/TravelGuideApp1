2541eac96db2f1628022032beff673a707200541
Özlem Miğfer <ozlemhayalkurun@gmail.com>
2023-06-09 20:34:55 +0300

    #51 Aktiviteler Ekranı-Seçilen şehir dropdown'un başlık olarak gösterilmesi.
    #51 Aktiviteler Ekranı-Seçilen şehir dropdown'un başlık olarak gösterilmesi.

--- src/pages/user/ActivitiesList/ActivitiesList.js
+++ src/pages/user/ActivitiesList/ActivitiesList.js
@@ -1,25 +1,33 @@
-import React from "react";
-import {Text, View, Image, TouchableOpacity} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {Text, View, Image, TouchableOpacity, ActivityIndicator} from 'react-native';
 import styles from './ActivitiesList.styles';
 
+const ActivitiesList = ({route, brand, amount}) => {
+  const [activity, setActivity] = useState('');
+  const [selectedActivity, setSelectedActivity] = useState('');
+  const [keyActivity, setKeyActivity] = useState(null);
 
-const ActivitiesList = () => {
-    return(
-        <View style={styles.container}>
-        <Image
-          source={require('../../../components/assets/back-gray-blur.jpg')}
-          style={styles.background}
-        />
-        <View style={styles.header}>
-        <Text style={styles.cityName}>Elazığ</Text>
-        </View>
-        <TouchableOpacity
-              style={styles.body_container}
->
-              <Text style={styles.name}>Aktivite Adı</Text>
-            </TouchableOpacity>
-        </View>
-    );
+  const [loading, setLoading] = useState(true);
+
+
+  if (loading) {
+    <ActivityIndicator color="green" size="large" />;
+  }
+
+  return (
+    <View style={styles.container}>
+      <Image
+        source={require('../../../components/assets/back-gray-blur.jpg')}
+        style={styles.background}
+      />
+      <View style={styles.header}>
+        <Text style={styles.cityName}>{route.params?.selectedCity}</Text>
+      </View>
+      <TouchableOpacity style={styles.body_container}>
+        <Text style={styles.name}>Aktivite Adı</Text>
+      </TouchableOpacity>
+    </View>
+  );
 };
 
 export default ActivitiesList;
