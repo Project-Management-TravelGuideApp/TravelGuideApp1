e83eb30b3c675d66f7905beb162bb5e412012222
Özlem Miğfer <ozlemhayalkurun@gmail.com>
2023-06-10 18:43:34 +0300

    #61 Aktivite Detay Ekranı-ScrollView yapısıyla çekilen verilerin listelenmesi.
    #61 Aktivite Detay Ekranı-ScrollView yapısıyla çekilen verilerin listelenmesi.

--- src/pages/user/DetailPage/DetailPage.js
+++ src/pages/user/DetailPage/DetailPage.js
@@ -1,9 +1,193 @@
-import React from 'react';
-import {Text, View, Image, TouchableOpacity} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {
+  Text,
+  View,
+  Image,
+  Alert,
+  ActivityIndicator,
+  TouchableOpacity,
+  ScrollView,
+} from 'react-native';
 import styles from './DetailPage.styles';
 import {ActivityDetail} from '../../../services/activitiesDetail';
 
-const DetailPage = () => {
+const DetailPage = ({route}) => {
+  // console.log('seçilen aktivite :',route.params);
+  const [activity, setActivity] = useState('');
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (route.params == 'Hazar Gölü Tekne Turu') {
+      ActivityDetail.getHazarTekneTuru()
+        .then(act => {
+          setActivity(act), setLoading(false);
+        })
+        .catch(err => Alert.alert(err.code, err.message));
+    } else if (route.params == 'Hazarbaba Kayak Merkezi') {
+      ActivityDetail.getHazarKayak()
+        .then(act => {
+          setActivity(act), setLoading(false);
+        })
+        .catch(err => Alert.alert(err.code, err.message));
+    } else if (route.params == 'Kano') {
+      ActivityDetail.getKano()
+        .then(act => {
+          setActivity(act), setLoading(false);
+        })
+        .catch(err => Alert.alert(err.code, err.message));
+    } else if (route.params == 'Macera Parkı') {
+      ActivityDetail.getMaceraParkı()
+        .then(act => {
+          setActivity(act), setLoading(false);
+        })
+        .catch(err => Alert.alert(err.code, err.message));
+    } else if (route.params == 'Rafting') {
+      ActivityDetail.getRafting()
+        .then(act => {
+          setActivity(act), setLoading(false);
+        })
+        .catch(err => Alert.alert(err.code, err.message));
+    } else if (route.params == 'Zipline') {
+      ActivityDetail.getZipline()
+        .then(act => {
+          setActivity(act), setLoading(false);
+        })
+        .catch(err => Alert.alert(err.code, err.message));
+    }
+    //Tarihi Yerler
+    else if (route.params == 'Cimşit Bey Hamamı') {
+      ActivityDetail.getCimsit()
+        .then(act => {
+          setActivity(act), setLoading(false);
+        })
+        .catch(err => Alert.alert(err.code, err.message));
+    } else if (route.params == 'Dabakhane Mescidi') {
+      ActivityDetail.getDabakhane()
+        .then(act => {
+          setActivity(act), setLoading(false);
+        })
+        .catch(err => Alert.alert(err.code, err.message));
+    } else if (route.params == 'Harput Kalesi') {
+      ActivityDetail.getHarputKalesi()
+        .then(act => {
+          setActivity(act), setLoading(false);
+        })
+        .catch(err => Alert.alert(err.code, err.message));
+    } else if (route.params == 'Harput Ulu Cami') {
+      ActivityDetail.getHarputUluCami()
+        .then(act => {
+          setActivity(act), setLoading(false);
+        })
+        .catch(err => Alert.alert(err.code, err.message));
+    } else if (route.params == 'Kömürhan Köprüsü') {
+      ActivityDetail.getKomurhan()
+        .then(act => {
+          setActivity(act), setLoading(false);
+        })
+        .catch(err => Alert.alert(err.code, err.message));
+    } else if (route.params == 'Maden Saat Kulesi') {
+      ActivityDetail.getMaden()
+        .then(act => {
+          setActivity(act), setLoading(false);
+        })
+        .catch(err => Alert.alert(err.code, err.message));
+    } else if (route.params == 'Palu Kalesi') {
+      ActivityDetail.getPaluKalesi()
+        .then(act => {
+          setActivity(act), setLoading(false);
+        })
+        .catch(err => Alert.alert(err.code, err.message));
+    } else if (route.params == 'Palu Surp Lusavoric Kilisesi') {
+      ActivityDetail.getPaluSurp()
+        .then(act => {
+          setActivity(act), setLoading(false);
+        })
+        .catch(err => Alert.alert(err.code, err.message));
+    } else if (route.params == 'Tarihi Palu Köprüsü') {
+      ActivityDetail.getPaluKoprusu()
+        .then(act => {
+          setActivity(act), setLoading(false);
+        })
+        .catch(err => Alert.alert(err.code, err.message));
+    } else if (route.params == 'İzzet Paşa Camii') {
+      ActivityDetail.getIzzet()
+        .then(act => {
+          setActivity(act), setLoading(false);
+        })
+        .catch(err => Alert.alert(err.code, err.message));
+    } else if (route.params == 'Şefik Gül Kültür Evi') {
+      ActivityDetail.getSefik()
+        .then(act => {
+          setActivity(act), setLoading(false);
+        })
+        .catch(err => Alert.alert(err.code, err.message));
+    } else if (route.params == 'Arkeoloji ve Etnoğrafya Müzesi') {
+      ActivityDetail.getEtnografMuze()
+        .then(act => {
+          setActivity(act), setLoading(false);
+        })
+        .catch(err => Alert.alert(err.code, err.message));
+    } else if (route.params == 'Harput Musiki Müzesi') {
+      ActivityDetail.getMusikiMuze()
+        .then(act => {
+          setActivity(act), setLoading(false);
+        })
+        .catch(err => Alert.alert(err.code, err.message));
+    } else if (route.params == 'Ciğerci Kamil Usta') {
+      ActivityDetail.getKamil()
+        .then(act => {
+          setActivity(act), setLoading(false);
+        })
+        .catch(err => Alert.alert(err.code, err.message));
+    } else if (route.params == 'Elaziz Mutfağı') {
+      ActivityDetail.getElaziz()
+        .then(act => {
+          setActivity(act), setLoading(false);
+        })
+        .catch(err => Alert.alert(err.code, err.message));
+    } else if (route.params == 'Keyfet Steak House') {
+      ActivityDetail.getKeyfet()
+        .then(act => {
+          setActivity(act), setLoading(false);
+        })
+        .catch(err => Alert.alert(err.code, err.message));
+    } else if (route.params == 'Koçoğlu Restaurant') {
+      ActivityDetail.getKocoglu()
+        .then(act => {
+          setActivity(act), setLoading(false);
+        })
+        .catch(err => Alert.alert(err.code, err.message));
+    } else if (route.params == 'Mangal Vadisi') {
+      ActivityDetail.getMangal()
+        .then(act => {
+          setActivity(act), setLoading(false);
+        })
+        .catch(err => Alert.alert(err.code, err.message));
+    } else if (route.params == 'Orjin İskender') {
+      ActivityDetail.getOrjin()
+        .then(act => {
+          setActivity(act), setLoading(false);
+        })
+        .catch(err => Alert.alert(err.code, err.message));
+    } else if (route.params == 'Selo Usta Tatlıları') {
+      ActivityDetail.getSelo()
+        .then(act => {
+          setActivity(act), setLoading(false);
+        })
+        .catch(err => Alert.alert(err.code, err.message));
+    } else if (route.params == 'Hisar Kasap Izgara') {
+      ActivityDetail.getHisar()
+        .then(act => {
+          setActivity(act), setLoading(false);
+        })
+        .catch(err => Alert.alert(err.code, err.message));
+    }
+  });
+
+  if (loading) {
+    <ActivityIndicator color="green" size="large" />;
+  }
+
   return (
     <View style={styles.container}>
       <Image
@@ -11,19 +195,28 @@ const DetailPage = () => {
         style={styles.background}
       />
 
-      <View style={styles.body_container}>
-        <View style={styles.image_container}>
-          <TouchableOpacity style={styles.maps}>
-            <Image
-              style={styles.img_map}
-              source={require('../../../components/assets/maps.jpg')}
-            />
-          </TouchableOpacity>
-          <Image style={styles.image} />
-        </View>
-        <Text style={styles.header}></Text>
-        <Text style={styles.description}></Text>
-      </View>
+      <ScrollView>
+        {activity &&
+          activity.map((data, index) => (
+            <View style={styles.body_container} key={index}>
+              <View stylr={styles.image_container}>
+                <TouchableOpacity
+                  style={styles.maps}
+                  >
+                  <Image
+                    style={styles.img_map}
+                    source={require('../../../components/assets/maps.jpg')}
+                  />
+                </TouchableOpacity>
+                <Image style={styles.image} source={{uri: data.img}} />
+              </View>
+              <Text style={styles.header}>{route.params}</Text>
+              <Text style={styles.description} adjustsFontSizeToFit={true}>
+                {data.description}
+              </Text>
+            </View>
+          ))}
+      </ScrollView>
     </View>
   );
 };
