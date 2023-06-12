ba4cc41bef9a3849b0a2a37136c038716087fb2e
Özlem Miğfer <83713406+ZeynepUzmez@users.noreply.github.com>
2023-06-12 16:59:55 +0300

    #63 Rota Ekranı-MapView background yapısının biçimlendirilmesi.
    #63 Rota Ekranı-MapView background yapısının biçimlendirilmesi.

--- src/pages/user/MapPage/MapPage.js
+++ src/pages/user/MapPage/MapPage.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { Text, View } from "react-native";
+import styles from './MapPage.styles';
 
 const MapPage = () => {
     return(
-        <View>
+        <View style={styles.container}>
             <Text>MapPage</Text>
         </View>
     );
