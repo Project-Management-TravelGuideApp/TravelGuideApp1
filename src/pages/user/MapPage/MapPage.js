4cd635c985756cf20af5180181b7c4f52e412565
Özlem Miğfer <83713406+ZeynepUzmez@users.noreply.github.com>
2023-07-02 00:49:58 +0300

    #69 Rota Ekranı-Kullanıcı konumunun ve aktivitenin konumunun arasındaki menzil hesaplanarak rotanın oluşturulması.
    #69 Rota Ekranı-Kullanıcı konumunun ve aktivitenin konumunun arasındaki menzil hesaplanarak rotanın oluşturulması.

--- src/pages/user/MapPage/MapPage.js
+++ src/pages/user/MapPage/MapPage.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {Text, View} from 'react-native';
 import styles from './MapPage.styles';
 import MapView, {Marker} from 'react-native-maps';
+import MapViewDirections from 'react-native-maps-directions';
 
 const markers = [
   {
@@ -12,7 +13,7 @@ const markers = [
     },
   },
   {
-    title: 'harput Kalesi',
+    title: 'Harput Kalesi',
     coordinates: {
       latitude: 38.70434088711537,
       longitude: 39.25735231169615,
@@ -38,6 +39,13 @@ const MapPage = () => {
             title={marker.title}
           />
         ))}
+        <MapViewDirections
+          origin={markers[0].coordinates}
+          destination={markers[1].coordinates}
+          apikey={"AIzaSyCQf6aLMv3SUZx2jzERk7Stei1lc0Ge7Yg"}
+          strokeWidth={3}
+          strokeColor="hotpink"
+        />
       </MapView>
     </View>
   );
